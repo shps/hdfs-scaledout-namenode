30ede1e30b0a2bd51f132573d00b853198e406e3
Wasif Malik <wmalik@gmail.com>
2012-04-30 11:02:25 +0200

    DBAdmin cleanup

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/DBAdmin.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/DBAdmin.java
@@ -21,43 +21,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**Used for performing administrative functions on the database. 
- * Assumes that a MySQL API server is running in the cluster
  * @author wmalik
- *
  */
 public class DBAdmin {
  static final Log LOG = LogFactory.getLog(DBAdmin.class);
-	/**
-	 * @param database
-	 * @deprecated use DBAdmin.deleteAllTables
-	 */
-	@Deprecated
-	public static void truncateAllTables(String database) {
-		try {
-			   Class.forName("com.mysql.jdbc.Driver").newInstance();
-
-			   String ConnectionString="jdbc:mysql://" + "cloud3.sics.se:3307" + "/" + database + "?user=" +
-			          "wasif5" + "&password=" + "wasif";
-			   LOG.info("Truncating " + database + " on " + ConnectionString);
-			   Connection conn = DriverManager.getConnection(ConnectionString);
-			   Statement stmt=conn.createStatement();
-			   stmt.execute("call trunc_kthfs()");
-                           stmt.close();
-                           conn.close();
-			}
-			catch(SQLException SQLEx) {
-			   LOG.info("MySQL error: "+SQLEx.getErrorCode()+
-			          " SQLSTATE:" +SQLEx.getSQLState());
-			   LOG.info(SQLEx.getMessage());
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		
-	}
 	
 	/** Deletes all rows from all kthfs tables
 	 * @param session
