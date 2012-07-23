7c5aaac387a2de0072a0cb2784cec2598bbc7373
Kamal Hakimzadeh <kamal@sics.se>
2012-07-23 15:21:53 +0200

    cont..

---
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/persistance/data_access/entity/EntityDataAccess.java
@@ -0,0 +1,39 @@
+package org.apache.hadoop.hdfs.server.namenode.persistance.data_access.entity;
+
+import java.sql.SQLException;
+import java.sql.SQLTransientException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hdfs.server.namenode.persistance.storage.StorageException;
+
+/**
+ *
+ * @author kamal hakimzadeh <kamal@sics.se>
+ */
+public abstract class EntityDataAccess {
+
+  private static Log log = LogFactory.getLog(EntityDataAccess.class);
+
+  protected void handleSQLException(SQLException ex) throws StorageException {
+    if (ex instanceof SQLTransientException) {
+      throw new StorageException(ex);
+    }
+
+    String code = ex.getSQLState().substring(0, 2);
+
+    if (code.equals("01")) //Warning
+    {
+      log.warn(ex);
+    } else if (code.equals("24")) //Invalid cursor state
+    {
+      return;
+    } else if (code.equals("25") //Invalid Transaction State
+            || code.equals("40")) //Transaction Rollback
+    {
+      throw new StorageException(ex);
+    } else {
+      throw new RuntimeException(ex);
+    }
+
+  }
+}
