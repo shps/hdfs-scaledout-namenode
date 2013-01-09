fb12e1b17beabd4838909c84bd5afcbc63ffb48f
Jim Dowling <jdowling@kth.se>
2013-01-09 12:58:29 +0100

    chef

--- scripts/chef-repo/cookbooks/chefserver/recipes/default.rb
+++ scripts/chef-repo/cookbooks/chefserver/recipes/default.rb
@@ -225,6 +225,7 @@ bash "install_chef_ruby" do
 user "#{node[:chef][:user]}"
 ignore_failure false
 code <<-EOF
+source /etc/profile.d/rvm.sh
 
 sudo su -l #{node[:chef][:user]} -c "rvm user all; rvm install 1.9.3; rvm use 1.9.3 --default"
 #sudo su - #{node[:chef][:user]} -l -c "rvm install 1.9.2; rvm use 1.9.2 --default"
@@ -450,6 +451,8 @@ code <<-EOF
 
 CHEF_HOME=#{HomeDir}
 CHEF_HOMEBASE=$CHEF_HOME/homebase
+
+echo "export CHEF_USERNAME=#{node[:chef][:user]}" >> /etc/profile 
 echo "export CHEF_HOMEBASE=#{HomeDir}/homebase" >> /etc/profile 
 cd $CHEF_HOME
 git clone https://github.com/infochimps-labs/ironfan-homebase homebase
@@ -468,12 +471,18 @@ ln -sni $CHEF_HOMEBASE/knife $CHEF_HOME/.chef
 #      {username}.pem
 #      {organization}-validator.pem
 
-#rm -rf $CHEF_HOMEBASE/knife/credentials
-#mv $CHEF_HOMEBASE/knife/credentials/knife-user-example.rb $CHEF_HOMEBASE/knife/credentials/knife-user-#{node[:chef][:user]}.rb
+rm -rf $CHEF_HOMEBASE/knife/credentials
 cp -a $CHEF_HOMEBASE/knife/example-credentials $CHEF_HOMEBASE/knife/credentials
 cp /etc/chef/webui.pem $CHEF_HOMEBASE/knife/credentials/#{node[:chef][:client]}.pem
 cp /etc/chef/validation.pem $CHEF_HOMEBASE/knife/credentials/#{node[:chef][:org]}-validator.pem
 sudo chown -R #{node[:chef][:org]} $CHEF_HOMEBASE/knife/credentials/
+cd $CHEF_HOMEBASE/knife/credentials/
+mkdir certificates
+mkdir client_keys
+mkdir data_bag_keys
+mkdir ec2_certs
+mkdir ec2_keys
+
 mkdir -p $CHEF_HOMEBASE/tmp/.ironfan-clusters
 # cd $CHEF_HOMEBASE
 # knife cookbook upload -a
@@ -484,6 +493,13 @@ mkdir -p $CHEF_HOMEBASE/tmp/.ironfan-clusters
 touch $CHEF_HOMEBASE/.installed
 EOF
 not_if "test -f #{HomeDir}/homebase/.installed"
+end
+
+template "#{HomeDir}/homebase/knife/credentials/knife-org.rb" do
+  source "knife-org.rb.erb"
+  owner node[:chef][:user]
+  group node[:chef][:user]
+  mode 0755
 end
 
 template "#{HomeDir}/homebase/knife/credentials/knife-user-#{node[:chef][:user]}.rb" do
