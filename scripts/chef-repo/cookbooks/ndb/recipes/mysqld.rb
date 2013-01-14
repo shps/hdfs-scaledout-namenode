dc86aeb30bf3f353f1a6ab97a1fa3b237f1f4d31
Jim Dowling <jdowling@sics.se>
2013-01-14 17:17:33 +0100

    cleaned up ndb recipe to execute in a single run

--- scripts/chef-repo/cookbooks/ndb/recipes/mysqld.rb
+++ scripts/chef-repo/cookbooks/ndb/recipes/mysqld.rb
@@ -90,7 +90,7 @@ template "/etc/init.d/mysqld" do
               :mysql_dir => node[:mysql][:base_dir],
               :connect_string => node[:ndb][:connect_string]
             })
+ notifies :install_distributed_privileges, resources(:ndb_mysql_start => "install")
  notifies :enable, resources(:service => "mysqld")
  notifies :restart, resources(:service => "mysqld"), :immediately
- notifies :install_distributed_privileges, resources(:ndb_mysql_start => "install"), :immediately
 end
