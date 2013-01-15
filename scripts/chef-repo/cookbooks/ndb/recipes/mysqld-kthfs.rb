adcec23650aef46981cda37b4615127cd5c593f2
Jim Dowling <jdowling@kth.se>
2013-01-15 14:50:42 +0100

    vagrantFile port forwarding restricted to only active recipes

--- scripts/chef-repo/cookbooks/ndb/recipes/mysqld-kthfs.rb
+++ scripts/chef-repo/cookbooks/ndb/recipes/mysqld-kthfs.rb
@@ -1,6 +1,14 @@
 include 'kthfs'
+#include_recipe "collectd"
+include "collectd"
 
 ndb_kthfs_services "#{node[:ndb][:kthfs_services]}" do
  node_id node[:mysql][:id]
  action :install_mysqld
 end
+
+# TODO -COLLECTD - conf file update
+# collectd_plugin "dbi" do
+#   options :data_dir=>"/var/lib/collectd/rrd"
+# end
+ 
