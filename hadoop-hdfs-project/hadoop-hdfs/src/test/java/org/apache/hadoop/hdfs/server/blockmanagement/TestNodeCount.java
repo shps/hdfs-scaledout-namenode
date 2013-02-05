874acd2e0dd82611fd6d11e24f4990fbf7478315
Hooman Peiro <shps@sics.se>
2013-02-05 10:04:40 +0100

    TestNodeCount.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/blockmanagement/TestNodeCount.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/blockmanagement/TestNodeCount.java
@@ -21,6 +21,8 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import junit.framework.TestCase;
 
@@ -32,9 +34,13 @@ import org.apache.hadoop.hdfs.HdfsConfiguration;
 import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.apache.hadoop.hdfs.MiniDFSCluster.DataNodeProperties;
 import org.apache.hadoop.hdfs.protocol.Block;
-import org.apache.hadoop.hdfs.server.namenode.persistance.DBConnector;
 import org.apache.hadoop.hdfs.server.namenode.FSNamesystem;
+import org.apache.hadoop.hdfs.server.namenode.lock.TransactionLockManager;
+import org.apache.hadoop.hdfs.server.namenode.lock.TransactionLockManager.LockType;
 import org.apache.hadoop.hdfs.server.namenode.persistance.EntityManager;
+import org.apache.hadoop.hdfs.server.namenode.persistance.PersistanceException;
+import org.apache.hadoop.hdfs.server.namenode.persistance.RequestHandler.OperationType;
+import org.apache.hadoop.hdfs.server.namenode.persistance.TransactionalRequestHandler;
 
 /**
  * Test if live nodes count per node is correct 
@@ -44,7 +50,8 @@ import org.apache.hadoop.hdfs.server.namenode.persistance.EntityManager;
  * because they are detecting transient states.
  */
 public class TestNodeCount extends TestCase {
-  final short REPLICATION_FACTOR = (short)2;
+
+  final short REPLICATION_FACTOR = (short) 2;
   final long TIMEOUT = 60000L;
   long timeout = 0;
   long failtime = 0;
@@ -54,90 +61,92 @@ public class TestNodeCount extends TestCase {
   public void testNodeCount() throws Exception {
     // start a mini dfs cluster of 2 nodes
     final Configuration conf = new HdfsConfiguration();
-    final MiniDFSCluster cluster = 
-      new MiniDFSCluster.Builder(conf).numNameNodes(1).numDataNodes(REPLICATION_FACTOR).build();
+    final MiniDFSCluster cluster =
+            new MiniDFSCluster.Builder(conf).numDataNodes(REPLICATION_FACTOR).build();
     try {
       final FSNamesystem namesystem = cluster.getNamesystem();
       final BlockManager bm = namesystem.getBlockManager();
       final HeartbeatManager hm = bm.getDatanodeManager().getHeartbeatManager();
       final FileSystem fs = cluster.getFileSystem();
-      
+
       // populate the cluster with a one block file
       final Path FILE_PATH = new Path("/testfile");
       DFSTestUtil.createFile(fs, FILE_PATH, 1L, REPLICATION_FACTOR, 1L);
       DFSTestUtil.waitReplication(fs, FILE_PATH, REPLICATION_FACTOR);
       //ExtendedBlock block = DFSTestUtil.getFirstBlock(fs, FILE_PATH); // [1] This is a bug in this test case:  cannot be cast to Comparable
-      Block block = DFSTestUtil.getFirstBlock(fs, FILE_PATH).getLocalBlock();
+      final Block block = DFSTestUtil.getFirstBlock(fs, FILE_PATH).getLocalBlock();
 
       // keep a copy of all datanode descriptor
       final DatanodeDescriptor[] datanodes = hm.getDatanodes();
-      
+
       // start two new nodes
       cluster.startDataNodes(conf, 2, true, null, null);
       cluster.waitActive();
-      
+
       // bring down first datanode
       DatanodeDescriptor datanode = datanodes[0];
       DataNodeProperties dnprop = cluster.stopDataNode(datanode.getName());
-      
-      // make sure that NN detects that the datanode is down
-      try {
-        namesystem.writeLock();
-        synchronized (hm) {
-          datanode.setLastUpdate(0); // mark it dead
-          hm.heartbeatCheck();
-        }
-      } finally {
-        namesystem.writeUnlock();
+
+      synchronized (hm) {
+        datanode.setLastUpdate(0); // mark it dead
+        hm.heartbeatCheck();
       }
-      
+
       // the block will be replicated
       DFSTestUtil.waitReplication(fs, FILE_PATH, REPLICATION_FACTOR);
 
       // restart the first datanode
-      System.out.println("Restarting datanode: "+datanode.getStorageID());
       cluster.restartDataNode(dnprop);
       cluster.waitActive();
-      
+
       // check if excessive replica is detected (transient)
       initializeTimeout(TIMEOUT);
-      
       //while (countNodes(block.getLocalBlock(), namesystem).excessReplicas() == 0) {
       while (countNodes(block, namesystem).excessReplicas() == 0) { // Modification in changes to the bug [1]
         checkTimeout("excess replicas not detected");
       }
-      
-      // find out a non-excess node
-      List<DatanodeDescriptor> dataNodes = bm.getDatanodes(bm.getStoredBlock(block));
-      DatanodeDescriptor nonExcessDN = null;
-      EntityManager em = EntityManager.getInstance();
-      for (DatanodeDescriptor dn : dataNodes) {
-        Collection<Long> excessReplicas = em.findExcessReplicaByStorageId(dn.getStorageID());
-        if (excessReplicas == null || !excessReplicas.contains(block.getBlockId())) {
-          nonExcessDN = dn;
-          break;
+
+      TransactionalRequestHandler handler = new TransactionalRequestHandler(OperationType.TEST_NODE_COUNT) {
+
+        @Override
+        public Object performTask() throws PersistanceException, IOException {
+          // find out a non-excess node
+          List<DatanodeDescriptor> dataNodes = bm.getDatanodes(bm.getStoredBlock(block));
+          DatanodeDescriptor nonExcessDN = null;
+          for (DatanodeDescriptor dn : dataNodes) {
+            Collection<ExcessReplica> excessReplicas =
+                    EntityManager.findList(ExcessReplica.Finder.ByStorageId, dn.getStorageID());
+            ExcessReplica searchKey = new ExcessReplica(dn.getStorageID(), block.getBlockId());
+            if (excessReplicas == null || !excessReplicas.contains(searchKey)) {
+              nonExcessDN = dn;
+              break;
+            }
+          }
+          return nonExcessDN;
         }
-      }
-      assertTrue(nonExcessDN!=null);
-      
+
+        @Override
+        public void acquireLock() throws PersistanceException, IOException {
+          throw new UnsupportedOperationException("Not supported yet.");
+        }
+      };
+
+      DatanodeDescriptor nonExcessDN = (DatanodeDescriptor) handler.handle();
+      assertTrue(nonExcessDN != null);
+
       // bring down non excessive datanode
       dnprop = cluster.stopDataNode(nonExcessDN.getName());
       // make sure that NN detects that the datanode is down
-      
-      try {
-        namesystem.writeLock();
-        synchronized(hm) {
-          nonExcessDN.setLastUpdate(0); // mark it dead
-          hm.heartbeatCheck();
-        }
-      } finally {
-        namesystem.writeUnlock();
+
+      synchronized (hm) {
+        nonExcessDN.setLastUpdate(0); // mark it dead
+        hm.heartbeatCheck();
       }
 
       // The block should be replicated
       initializeTimeout(TIMEOUT);
       //while (countNodes(block.getLocalBlock(), namesystem).liveReplicas() != REPLICATION_FACTOR) { // Modification in changes to the bug [1]
-      while (countNodes(block, namesystem).liveReplicas() != REPLICATION_FACTOR) { 
+      while (countNodes(block, namesystem).liveReplicas() != REPLICATION_FACTOR) {
         checkTimeout("live replica count not correct", 1000);
       }
 
@@ -156,26 +165,26 @@ public class TestNodeCount extends TestCase {
       cluster.shutdown();
     }
   }
-  
+
   void initializeTimeout(long timeout) {
     this.timeout = timeout;
     this.failtime = System.currentTimeMillis()
-        + ((timeout <= 0) ? Long.MAX_VALUE : timeout);
+            + ((timeout <= 0) ? Long.MAX_VALUE : timeout);
   }
-  
+
   /* busy wait on transient conditions */
   void checkTimeout(String testLabel) throws TimeoutException {
     checkTimeout(testLabel, 0);
   }
-  
+
   /* check for timeout, then wait for cycleTime msec */
   void checkTimeout(String testLabel, long cycleTime) throws TimeoutException {
     if (System.currentTimeMillis() > failtime) {
       throw new TimeoutException("Timeout: "
-          + testLabel + " for block " + lastBlock + " after " + timeout 
-          + " msec.  Last counts: live = " + lastNum.liveReplicas()
-          + ", excess = " + lastNum.excessReplicas()
-          + ", corrupt = " + lastNum.corruptReplicas());   
+              + testLabel + " for block " + lastBlock + " after " + timeout
+              + " msec.  Last counts: live = " + lastNum.liveReplicas()
+              + ", excess = " + lastNum.excessReplicas()
+              + ", corrupt = " + lastNum.corruptReplicas());
     }
     if (cycleTime > 0) {
       try {
@@ -187,19 +196,30 @@ public class TestNodeCount extends TestCase {
   }
 
   /* threadsafe read of the replication counts for this block */
-  NumberReplicas countNodes(Block block, FSNamesystem namesystem) {
-    namesystem.readLock();
+  NumberReplicas countNodes(final Block block, final FSNamesystem namesystem) {
     try {
-      DBConnector.beginTransaction();
-      lastBlock = block;
-      lastNum = namesystem.getBlockManager().countNodes(block);
-      DBConnector.commit();
-      return lastNum;
-    }
-    catch (IOException ex) {
+      return (NumberReplicas) new TransactionalRequestHandler(OperationType.COUNT_NODES) {
+
+        @Override
+        public Object performTask() throws PersistanceException, IOException {
+          lastBlock = block;
+          lastNum = namesystem.getBlockManager().countNodes(block);
+          return lastNum;
+        }
+
+        @Override
+        public void acquireLock() throws PersistanceException, IOException {
+          TransactionLockManager lm = new TransactionLockManager();
+          lm.addBlock(TransactionLockManager.LockType.READ, block.getBlockId()).
+                  addReplica(LockType.READ).
+                  addExcess(LockType.READ).
+                  addCorrupt(LockType.READ);
+          lm.acquire();
+        }
+      }.handleWithReadLock(namesystem);
+    } catch (IOException ex) {
+      Logger.getLogger(TestNodeCount.class.getName()).log(Level.SEVERE, null, ex);
       return null;
-    }    finally {
-      namesystem.readUnlock();
     }
   }
 }
