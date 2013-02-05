e72f1cc875eda3b96c9151c7678c90de4c19ccff
Hooman Peiro <shps@sics.se>
2013-02-05 10:12:20 +0100

    TestPEndingReplication.

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/blockmanagement/TestPendingReplication.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/blockmanagement/TestPendingReplication.java
@@ -17,121 +17,182 @@
  */
 package org.apache.hadoop.hdfs.server.blockmanagement;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.TestCase;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hdfs.HdfsConfiguration;
 
 import org.apache.hadoop.hdfs.protocol.Block;
-import org.apache.hadoop.hdfs.server.namenode.persistance.DBConnector;
-import org.apache.hadoop.hdfs.server.namenode.persistance.EntityManager;
+import org.apache.hadoop.hdfs.server.namenode.lock.TransactionLockAcquirer;
+import org.apache.hadoop.hdfs.server.namenode.lock.TransactionLockManager;
+import org.apache.hadoop.hdfs.server.namenode.persistance.PersistanceException;
+import org.apache.hadoop.hdfs.server.namenode.persistance.RequestHandler.OperationType;
+import org.apache.hadoop.hdfs.server.namenode.persistance.TransactionalRequestHandler;
+import org.apache.hadoop.hdfs.server.namenode.persistance.storage.StorageException;
+import org.apache.hadoop.hdfs.server.namenode.persistance.storage.StorageFactory;
 
 /**
  * This class tests the internals of PendingReplicationBlocks.java
  */
 public class TestPendingReplication extends TestCase {
-  public static final Log LOG = LogFactory.getLog(TestPendingReplication.class);  
+
+  public static final Log LOG = LogFactory.getLog(TestPendingReplication.class);
   final static int TIMEOUT = 3;     // 3 seconds
 
   public void testPendingReplication() {
-    PendingReplicationBlocks pendingReplications;
-    pendingReplications = new PendingReplicationBlocks(TIMEOUT * 1000);
-    
-    DBConnector.setConfiguration(new HdfsConfiguration());
-    DBConnector.formatDB();
-    DBConnector.beginTransaction();
-    EntityManager em = EntityManager.getInstance();
-    //
-    // Add 10 blocks to pendingReplications.
-    //
-    for (int i = 0; i < 10; i++) {
-      Block block = new Block(i, i, 0);
-      pendingReplications.add(block, i);
-    }
-    
-    assertEquals("Size of pendingReplications ",
-                 10, pendingReplications.size());
-    
-    DBConnector.commit(); //FIXME[H]: An improved transaction context is required to handle add and remove in the same transactions.
-    DBConnector.beginTransaction();
-    //
-    // remove one item and reinsert it
-    //
-    Block blk = new Block(8, 8, 0);
-    pendingReplications.remove(blk);             // removes one replica
-    assertEquals("pendingReplications.getNumReplicas ",
-                 7, pendingReplications.getNumReplicas(blk));
-
-    for (int i = 0; i < 7; i++) {
-      pendingReplications.remove(blk);           // removes all replicas
-    }
-    
-    assertTrue(pendingReplications.size() == 9);
-    
-    DBConnector.commit(); 
-    
-    DBConnector.beginTransaction(); //again wants to add the totally removed block
-    pendingReplications.add(blk, 8);
-    assertTrue(pendingReplications.size() == 10);
-
-    //
-    // verify that the number of replicas returned
-    // are sane.
-    //
-    for (int i = 0; i < 10; i++) {
-      Block block = new Block(i, i, 0);
-      int numReplicas = pendingReplications.getNumReplicas(block);
-      assertTrue(numReplicas == i);
-    }
-
-    //
-    // verify that nothing has timed out so far
-    //
-    assertTrue(pendingReplications.getTimedOutBlocks() == null);
-
-    //
-    // Wait for one second and then insert some more items.
-    //
     try {
-      Thread.sleep(1000);
-    } catch (Exception e) {
-    }
-
-    for (int i = 10; i < 15; i++) {
-      Block block = new Block(i, i, 0);
-      pendingReplications.add(block, i);
-    }
-    assertTrue(pendingReplications.size() == 15);
-
-    //
-    // Wait for everything to timeout.
-    //
-    int loop = 0;
-    while (pendingReplications.size() > 0) {
+      final PendingReplicationBlocks pendingReplications = new PendingReplicationBlocks(TIMEOUT * 1000);
+      StorageFactory.setConfiguration(new HdfsConfiguration());
+      StorageFactory.getConnector().formatStorage();
       try {
-        Thread.sleep(1000);
-      } catch (Exception e) {
-      }
-      loop++;
-    }
-    LOG.info("Had to wait for " + loop +
-                       " seconds for the lot to timeout");
+        TransactionalRequestHandler addPendingHandler = new TransactionalRequestHandler(OperationType.TEST_PENDING_REPLICATION) {
 
-    DBConnector.commit();
-    
-    DBConnector.beginTransaction();
-    //
-    // Verify that everything has timed out.
-    //
-    assertEquals("Size of pendingReplications ",
-                 0, pendingReplications.size());
-    List<PendingBlockInfo> timedOut = pendingReplications.getTimedOutBlocks();
-    assertTrue(timedOut != null && timedOut.size() == 15);
-    for (int i = 0; i < timedOut.size(); i++) {
-      assertTrue(timedOut.get(i).getBlockId() < 15);
+          @Override
+          public Object performTask() throws PersistanceException, IOException {
+            Block block = (Block) getParams()[0];
+            int replication = (Integer) getParams()[1];
+            pendingReplications.add(block, replication);
+            return null;
+          }
+
+          @Override
+          public void acquireLock() throws PersistanceException, IOException {
+            Block block = (Block) getParams()[0];
+            TransactionLockAcquirer.acquireLock(TransactionLockManager.LockType.WRITE, PendingBlockInfo.Finder.ByPKey, block.getBlockId());
+          }
+        };
+
+        //
+        // Add 10 blocks to pendingReplications.
+        //
+        for (int i = 0; i < 10; i++) {
+          Block block = new Block(i, i, 0);
+          addPendingHandler.setParams(block, i).handle();
+        }
+
+        assertEquals("Size of pendingReplications ",
+                10, pendingReplications.size(OperationType.TEST_PENDING_REPLICATION));
+
+        final Block blk = new Block(8, 8, 0);
+
+        new TransactionalRequestHandler(OperationType.TEST_PENDING_REPLICATION) {
+
+          @Override
+          public Object performTask() throws PersistanceException, IOException {
+            //
+            // remove one item and reinsert it
+            //
+            pendingReplications.remove(blk);             // removes one replica
+            assertEquals("pendingReplications.getNumReplicas ",
+                    7, pendingReplications.getNumReplicas(blk));
+
+            for (int i = 0; i < 7; i++) {
+              pendingReplications.remove(blk);           // removes all replicas
+            }
+
+
+            return null;
+          }
+
+          @Override
+          public void acquireLock() throws PersistanceException, IOException {
+            TransactionLockAcquirer.acquireLock(TransactionLockManager.LockType.WRITE, PendingBlockInfo.Finder.ByPKey, blk);
+          }
+        }.handle();
+        assertTrue(pendingReplications.size(OperationType.TEST_PENDING_REPLICATION) == 9);
+        new TransactionalRequestHandler(OperationType.TEST_PENDING_REPLICATION) {
+
+          @Override
+          public Object performTask() throws PersistanceException, IOException {
+            pendingReplications.add(blk, 8);
+            return null;
+          }
+
+          @Override
+          public void acquireLock() throws PersistanceException, IOException {
+            TransactionLockAcquirer.acquireLock(TransactionLockManager.LockType.WRITE, PendingBlockInfo.Finder.ByPKey, blk);
+          }
+        }.handle();
+
+        assertTrue(pendingReplications.size(OperationType.TEST_PENDING_REPLICATION) == 10);
+
+        TransactionalRequestHandler findPendingHandler = new TransactionalRequestHandler(OperationType.TEST_PENDING_REPLICATION) {
+
+          @Override
+          public Object performTask() throws PersistanceException, IOException {
+            Block block = (Block) getParams()[0];
+            return pendingReplications.getNumReplicas(block);
+          }
+
+          @Override
+          public void acquireLock() throws PersistanceException, IOException {
+            Block block = (Block) getParams()[0];
+            TransactionLockAcquirer.acquireLock(TransactionLockManager.LockType.READ, PendingBlockInfo.Finder.ByPKey, block.getBlockId());
+          }
+        };
+        //
+        // verify that the number of replicas returned
+        // are sane.
+        //
+        for (int i = 0; i < 10; i++) {
+          Block block = new Block(i, i, 0);
+          int numReplicas = (Integer) findPendingHandler.setParams(block).handle();
+          assertTrue(numReplicas == i);
+        }
+
+        //
+        // verify that nothing has timed out so far
+        //
+        assertTrue(pendingReplications.getTimedOutBlocks(OperationType.TEST_PENDING_REPLICATION4) == null);
+
+        //
+        // Wait for one second and then insert some more items.
+        //
+        try {
+          Thread.sleep(1000);
+        } catch (Exception e) {
+        }
+
+        for (int i = 10; i < 15; i++) {
+          Block block = new Block(i, i, 0);
+          addPendingHandler.setParams(block, i).handle();
+        }
+        assertTrue(pendingReplications.size(OperationType.TEST_PENDING_REPLICATION) == 15);
+
+        //
+        // Wait for everything to timeout.
+        //
+        int loop = 0;
+        while (pendingReplications.size(OperationType.TEST_PENDING_REPLICATION) > 0) {
+          try {
+            Thread.sleep(1000);
+          } catch (Exception e) {
+          }
+          loop++;
+        }
+        LOG.info("Had to wait for " + loop
+                + " seconds for the lot to timeout");
+        //
+        // Verify that everything has timed out.
+        //
+        assertEquals("Size of pendingReplications ",
+                0, pendingReplications.size(OperationType.TEST_PENDING_REPLICATION));
+
+        List<PendingBlockInfo> timedOut = pendingReplications.getTimedOutBlocks(OperationType.TEST_PENDING_REPLICATION4);
+        assertTrue(timedOut != null && timedOut.size() == 15);
+        for (int i = 0; i < timedOut.size(); i++) {
+          assertTrue(timedOut.get(i).getBlockId() < 15);
+        }
+      } catch (IOException ex) {
+        Logger.getLogger(TestPendingReplication.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
+    } catch (StorageException ex) {
+      Logger.getLogger(TestPendingReplication.class.getName()).log(Level.SEVERE, null, ex);
     }
-    
-    DBConnector.commit();
   }
 }
